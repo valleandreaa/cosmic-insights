0fb37570832a8bd37231bc3ac9332f7148d4e1a3
Predip <piradip_94@hotmail.com>
2023-12-19 12:29:37 +0100

    clean up

---
+++ src/asteroids/neural_network.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+
+# Define a simple neural network model
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.sigmoid(self.fc1(x))
+        x = self.sigmoid(self.fc2(x))
+        x = self.sigmoid(self.fc3(x))
+        return x
